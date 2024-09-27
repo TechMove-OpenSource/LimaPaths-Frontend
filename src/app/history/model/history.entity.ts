9f0a0e8ece2d1437bb4a9b0a5b7e9df788a52639
OmBRz <omarbrrcl@gmail.com>
2024-09-27 02:23:23 -0500

    feat(planning): added history components model and service.

---
+++ src/app/history/model/history.entity.ts
@@ -0,0 +1,12 @@
+export class Trip {
+  id: number;
+  start_date: Date;
+  end_date: Date;
+
+  constructor() {
+    this.id = 0;
+    this.start_date = new Date();
+    this.end_date = new Date();
+  }
+
+}
